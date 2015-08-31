99f7cce7ace54644f8a80c66dbd3bc0b0d7cbfec
idyczko <igor.dyczko@gmail.com>
2015-08-31 16:08:33 +0200

    Css changed, bundles added

--- imageviewer/src/com/capgemini/imageviewer/Main.java
+++ imageviewer/src/com/capgemini/imageviewer/Main.java
@@ -1,35 +1,43 @@
 package com.capgemini.imageviewer;
-	
+
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	private static final String BUNDLE_PATH = "com/capgemini/imageviewer/view/bundle/bundle";
+
 	private Stage primaryStage = new Stage();
-	
-	@Override 
+
+	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Image Viewer");
+
+		initImageViewerLayout();
+	}
+
+	private void initImageViewerLayout() {
 		try {
-	        FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/fxml/ImageViewer.fxml"));
-			BorderPane rootLayout= loader.load();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/fxml/ImageViewer.fxml"));
+			loader.setResources(ResourceBundle.getBundle(BUNDLE_PATH));
+			BorderPane rootLayout = loader.load();
 			Scene scene = new Scene(rootLayout);
 			scene.getStylesheets().add(getClass().getResource("view/css/application.css").toExternalForm());
 			this.primaryStage.setScene(scene);
 			this.primaryStage.show();
-		
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
-	
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
